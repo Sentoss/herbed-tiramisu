a3af51911e035f0c93e065b80fa2d92c72c5fbf0
Sentoss <34657765+Sentoss@users.noreply.github.com>
2023-07-16 18:53:42 +0300

    Update herbed-tiramisu.sh
    removed the timeout (uses normal herbe timeout), and made the message = body, in order to see more of the content.

--- herbed-tiramisu.sh
+++ herbed-tiramisu.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #thanks to anufrievroman for style (https://github.com/anufrievroman/polytiramisu/blob/9c0a039d8cd8b7066bccbbd237cd8939da66e1fb/polytiramisu.sh#L18)
-tiramisu -o "export timeout=#timeout app=#source message=#summary" |
+tiramisu -o "export app=#source message=#body" |
 
 while read -r tiramisu
 do	$tiramisu
-timeout -s9 ${timeout}s herbe "App: $app" "" "Message: $message"
+herbe "$app" "" "$message"
 done
